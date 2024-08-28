8542bd56f2ea63ff89bdbcd62429793f6d2656ac
lucas <lucas.chorolqui@gmail.com>
2024-08-28 18:35:53 -0300

    updates

---
+++ tests/app.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "@jest/globals";
+
+describe("App", () => {
+  // en cambio de it() tambien es comun usar test()
+  it("should be true", () => {
+    expect(true).toBe(true);
+  });
+});
